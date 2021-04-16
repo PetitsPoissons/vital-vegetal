0ad13aca2bea6b7ce1ecd4f4caab6d76cef5785b
Sandrine Poissonnet <petitspoissons@icloud.com>
2021-04-16 10:46:48 -0700

    functional appbar and scroll to top effect

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import { connect } from 'react-redux';
 import { fetchMe } from './actions/authActions';
 
 // Components
+import Header from './components/layout/Header';
 import Navbar from './components/layout/Navbar';
 import HomePage from './components/pages/HomePage';
 import WhyVeganPage from './components/pages/WhyVeganPage';
@@ -15,7 +16,54 @@ import RecipesPage from './components/pages/RecipesPage';
 import RecipePage from './components/pages/RecipePage';
 import AuthPage from './components/pages/AuthPage';
 
-const App = ({ fetchMe }) => {
+// Styles & Assets
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Fab from '@material-ui/core/Fab';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+}));
+
+function ScrollTop(props) {
+  const { children } = props;
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor'
+    );
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+const App = (props, { fetchMe }) => {
   useEffect(() => {
     if (localStorage.token) {
       fetchMe();
@@ -23,7 +71,9 @@ const App = ({ fetchMe }) => {
   }, []);
   return (
     <BrowserRouter>
-      <Navbar />
+      <CssBaseline />
+      <Header />
+      <Toolbar id="back-to-top-anchor" />
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/why-vegan" component={WhyVeganPage}></Route>
@@ -31,6 +81,11 @@ const App = ({ fetchMe }) => {
         <Route exact path="/recipe/:id" component={RecipePage}></Route>
         <Route exact path="/auth" component={AuthPage}></Route>
       </Switch>
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
     </BrowserRouter>
   );
 };
